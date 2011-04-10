643c89eaff3d8bda63cf86da86e5583d3a980556
sideris <sideris@gmail.com>
2011-04-10 01:23:36 +0100

    Fixed debug functionality

--- src/clarity/debug.clj
+++ src/clarity/debug.clj
@@ -1,6 +1,5 @@
 (ns clarity.debug)
 
 (defmacro debug-values [& args]
-  (let* [labels (map (fn [x] (str x ": ")) args)
-         everything (interleave labels args (repeat "\n"))]
-    `(print (str ~@everything))))
+  `(do ~@(map (fn [x]
+                `(print (str ~(str x) ": " (pr-str ~x) "\n"))) args)))
