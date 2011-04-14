2d4832582ccd96d503239e5c7fa6c7f9341fda01
sideris <sideris@gmail.com>
2011-04-14 19:25:11 +0100

    Added function to get L&F properties

--- src/clarity/utils.clj
+++ src/clarity/utils.clj
@@ -1,17 +1,21 @@
 (ns clarity.utils)
 
 
-(defn get-properties
-  ([] (get-properties nil))
+(defn get-laf-properties
+  ([] (get-laf-properties nil))
   ([regex]
      (let [defaults (javax.swing.UIManager/getLookAndFeelDefaults)]
        (if regex
-         (filter #(re-find regex (.toString (key %))) defaults)
+         (filter #(re-seq regex (.toString (key %))) defaults)
          defaults))))
 
-(defn show-properties
-  ([] (show-properties nil))
+(defn find-laf-properties
+  ([] (find-laf-properties nil))
   ([regex]
-     (let [matches (get-properties regex)]
+     (let [matches (get-laf-properties regex)]
        (doseq [entry matches]
          (print (key entry) ": " (val entry) "\n")))))
+
+(defn get-laf-property
+  [key]
+  (javax.swing.UIManager/get key))
