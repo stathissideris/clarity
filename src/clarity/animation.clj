cbe6a56e929a342296aec3ef0d57bce392a1f266
sideris <sideris@gmail.com>
2011-04-16 16:11:52 +0100

    Minor changes in animation

--- src/clarity/animation.clj
+++ src/clarity/animation.clj
@@ -1,24 +1,24 @@
 (ns clarity.animation)
 
 (defprotocol interpolatable
-  (interpolate [start end d]))
-;;  (interpolate [start end d function]))
+  (interpolate [start end t]))
+;;  (interpolate [start end t function]))
 
 (extend-type java.lang.Number
   interpolatable
-  (interpolate [start end d]
-               (+ start (* d (- end start)))))
+  (interpolate [start end t]
+               (+ start (* t (- end start)))))
 
 (extend-type clojure.lang.PersistentVector
   interpolatable
-  (interpolate [start end d]
-               (vec (map #(interpolate %1 %2 d) start end))))
+  (interpolate [start end t]
+               (vec (map #(interpolate %1 %2 t) start end))))
 
 (extend-type java.awt.Color
   interpolatable
   (interpolate [start end d]
                (new java.awt.Color
-                    (int (interpolate (.getRed start) (.getRed end) d))
-                    (int (interpolate (.getGreen start) (.getGreen end) d))
-                    (int (interpolate (.getBlue start) (.getBlue end) d))
-                    (int (interpolate (.getAlpha start) (.getAlpha end) d)))))
+                    (int (interpolate (.getRed start)   (.getRed end)   t))
+                    (int (interpolate (.getGreen start) (.getGreen end) t))
+                    (int (interpolate (.getBlue start)  (.getBlue end)  t))
+                    (int (interpolate (.getAlpha start) (.getAlpha end) t)))))
