d3133aa39a661d5eeec28de561a89314bc704288
Stathis Sideris <sideris@gmail.com>
2011-06-07 01:35:32 +0100

    Added simple form creation function

--- src/clarity/form.clj
+++ src/clarity/form.clj
@@ -1,6 +1,10 @@
-(ns clarity.form)
+(ns clarity.form
+  (:require [clojure.string :as str]
+            [clojure.contrib.miglayout :as mig]
+            [clarity.component :as component]
+            clarity.list))
 
-(defn interpose-every [sep coll every]
+(defn interpose-every [every sep coll]
   (let [piece (fn piece [sep coll every]
                 (when (and coll (not (empty? coll)))
                   (concat (take every coll)
@@ -8,6 +12,25 @@
                           (piece sep (drop every coll) every))))]
     (butlast (piece sep coll every))))
 
-(defn make-form [& components]
-  (let [pairs (interpose-every :wrap components 2)]
-    pairs))
+(defn field [param]
+  (cond (keyword? param)
+        (cond (= :number param) (component/make :text-field)
+              (= :string param) (component/make :text-field))
+        (string? param) (component/make (:text-field param))
+        (number? param) (component/make (:text-field (str param)))
+        (sequential? param) (component/make
+                             (:combo-box (to-array param)))))
+
+(defn label [l]
+  (component/make (:label
+                   (str/capitalize (str/replace l "-" " ")))))
+
+(defn make-simple-form [& components]
+  (apply mig/miglayout (component/make :panel)
+         :layout "wrap 2"
+         :column "[left][grow,fill]"
+         (reduce concat
+                 (map #(list (label (name (first %)))
+                             (field (second %))
+                             :sg) ;;to achieve equal heights
+                      (partition 2 components)))))
