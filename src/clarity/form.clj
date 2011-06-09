c395725bd6967b91d53bd9dfac15f1b92eebba41
Stathis Sideris <sideris@gmail.com>
2011-06-09 02:27:46 +0100

    Added sophisticated tokenization of the forms elements

--- src/clarity/form.clj
+++ src/clarity/form.clj
@@ -12,7 +12,33 @@
                           (piece sep (drop every coll) every))))]
     (butlast (piece sep coll every))))
 
-(defn field [param]
+(defn destructure-flags [flags & args]
+  {:flags (apply hash-set (filter flags args))
+   :args (apply hash-map (remove flags args))})
+
+(defn header? [x] (and (sequential? x) (some #(= :header %) x)))
+(defn group? [x] (and (sequential? x) (some #(= :group %) x)))
+(defn end-group? [x] (and (sequential? x) (some #(= :end-group %) x)))
+(defn text? [x] (and (sequential? x) (some #(= :text %) x)))
+
+(defn special-tag? [x]
+  (or (header? x)
+      (group? x)
+      (end-group? x)
+      (text? x)))
+
+(defn tokens [form]
+  (loop [f form
+         tok ()]
+    (cond (not (seq f)) (apply vector (reverse tok))
+          (special-tag? (first f)) (recur (next f) (conj tok (first f)))
+          (and (> (count f) 2)
+               (sequential? (nth f 2))
+               (not (special-tag? (nth f 2))))
+          (recur (next (nnext f)) (conj tok (apply vector (take 3 f))))
+          :else (recur (nnext f) (conj tok (apply vector (take 2 f)))))))
+
+(defn make-field [param]
   (cond (keyword? param)
         (cond (= :number param) (component/make :text-field)
               (= :string param) (component/make :text-field))
@@ -21,16 +47,16 @@
         (sequential? param) (component/make
                              (:combo-box (to-array param)))))
 
-(defn label [l]
+(defn make-label [l]
   (component/make (:label
                    (str/capitalize (str/replace l "-" " ")))))
 
-(defn simple-form [& components]
+(defn form [& components]
   (apply mig/miglayout (component/make :panel)
          :layout "wrap 2"
          :column "[left][grow,fill]"
          (reduce concat
-                 (map #(list (label (name (first %)))
-                             (field (second %))
+                 (map #(list (make-label (name (first %)))
+                             (make-field (second %))
                              :sg) ;;to achieve equal heights
                       (partition 2 components)))))
