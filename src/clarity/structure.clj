aa0292d2b9ddcf3330d8a40ae2d2ef054c14225c
Stathis Sideris <sideris@gmail.com>
2011-08-15 14:17:51 +0300

    More structure functions

--- src/clarity/structure.clj
+++ src/clarity/structure.clj
@@ -10,20 +10,34 @@
             (fn [node] (.getComponents node))
             root))
 
+(defn filter-by-category
+  [category coll]
+  (filter #(c/has-category % category) coll))
+
 (defn find-by-category
   "Finds recursively all the children of root that have the requested
   category. Root itself is included in the search."
   [root category]
-  (filter #(c/has-category % category) (comp-seq root)))
+  (filter-by-category category (comp-seq root)))
 
 ;;example
 #_(map #(.getText %) (find-by-category (clarity.form/form :a 6 :b 8) :form-label))
+
+(defn children-with-category
+  "Returns the direct descendants of parent with the matching category (if any)."
+  [parent category]
+  (filter-by-category category (.getComponents parent)))
 
 (defn find-by-id
   "Finds recursively the child of root (or root itself) that has the
   passed ID."
   [root id]
   (first (filter #(= id (c/id %)) (comp-seq root))))
+
+(defn child-with-id
+  "Returns a direct descendant of parent with a matching id (if any)."
+  [parent id]
+  (first (filter #(= id (c/id %)) (.getComponents parent))))
 
 ;;example
 #_(.getText (find-by-id (clarity.form/form :a 6 :b 7) :a))
@@ -36,3 +50,11 @@
     (let [parent (.getParent c)]
       (if (nil? parent) p
         (recur parent (conj p parent))))))
+
+;;;selectors
+
+;; (category :form
+;;           category :form-header
+;; 
+;; (filter-by-category :form-header
+;;                     (mapcat () (find-by-category :form))
