f5c61087d3a8dc50366632b5e158585717e7b037
Stathis Sideris <sideris@gmail.com>
2011-08-28 03:44:52 +0100

    Added support for mutable list models that include a transforming function to be applied on the watched data

--- src/clarity/list.clj
+++ src/clarity/list.clj
@@ -10,15 +10,29 @@
 (defn mutable-list-model
   "Constructs a list model that wraps the passed agent/atom/var/ref
   reference. Also adds a watch to the data so that any changes cause
-  the fireContentsChanged() method of the model to be called. This
-  works OK for short lists, but if the list will likely be large (or
-  change very often), you should consider writing a customised model."
-  [data]
-  (let [watch-key (gensym "list-watch-")
-        model (proxy [javax.swing.AbstractListModel] []
-               (getElementAt [index] (nth @data index))
-               (getSize [] (count @data)))]
-    (add-watch data watch-key
-               (fn [k r old new]
-                 (.fireContentsChanged model model 0 (dec (count new)))))
-    model))
+  the fireContentsChanged() method of the model to be called. If a
+  function is passed, it is applied to the reference before a
+  particular element being returned. This works OK for short lists,
+  but if the list will likely be large (or change very often), you
+  should consider writing a customised model."
+  
+  ([data]
+     (let [watch-key (gensym "list-watch-")
+           model (proxy [javax.swing.AbstractListModel] []
+                   (getElementAt [index] (nth @data index))
+                   (getSize [] (count @data)))]
+       (add-watch data watch-key
+                  (fn [k r old new]
+                    (.fireContentsChanged model model 0 (dec (count new)))))
+       model))
+  ([data fun]
+     (let [cache (atom (fun @data))
+           watch-key (gensym "list-watch-")
+           model (proxy [javax.swing.AbstractListModel] []
+                   (getElementAt [index] (nth @cache index))
+                   (getSize [] (count @cache)))]
+       (add-watch data watch-key
+                  (fn [k r old new]
+                    (reset! cache (fun new))
+                    (.fireContentsChanged model model 0 (dec (count new)))))
+       model)))
