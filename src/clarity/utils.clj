9748d3c7a8c63530b17cc8f3451873c1fadb87db
Stathis Sideris <sideris@gmail.com>
2011-09-03 14:37:33 +0100

    Added (watch-image) to utils to allow easy debugging of drawing to images.

--- src/clarity/utils.clj
+++ src/clarity/utils.clj
@@ -1,4 +1,5 @@
 (ns clarity.utils
+  (:require [clarity.component :as c])
   (:import [javax.swing UIManager JFrame]))
 
 (defmacro qw
@@ -15,6 +16,37 @@
     (.pack)
     (.setVisible true)))
 
+(defn watch-image
+  "Shows the passed java.awt.Image in a frame, and re-paints at 15
+  FPS (or the specified FPS). You can also pass a reference to an
+  Image, which will be dereferenced at every frame. The function
+  returns a future which can be cancelled to stop the re-painting. Of
+  course the re-painting stops automatically when the frame is
+  closed."
+  ([image] (watch-image image 15))
+  ([image fps]
+     (let [get-image (fn [] (if (instance? clojure.lang.IDeref image)
+                              @image image))
+           panel (proxy [javax.swing.JPanel] []
+                   (paintComponent [g] (if (get-image)
+                                         (.drawImage g (get-image) 0 0 this)))
+                   (getPreferredSize[] (if (get-image)
+                                         (java.awt.Dimension.
+                                          (.getWidth (get-image))
+                                          (.getHeight (get-image)))
+                                         (java.awt.Dimension. 100 100))))
+           updater (future
+                    (while true
+                      (Thread/sleep (/ 1000 fps))
+                      (.repaint panel)))]
+       (c/make :frame
+               (.add panel)
+               (.pack)
+               [:visible true]
+               [:on-window-closing
+                (future-cancel updater)])
+       updater)))
+       
 (defn dispose-all-frames []
   (doseq [frame (java.awt.Frame/getFrames)]
     (.dispose frame)))
