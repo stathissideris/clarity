d9ce08d3a5c024808342804f3c843df18d3aa933
Stathis Sideris <sideris@gmail.com>
2011-10-11 18:33:12 +0100

    Function that makes a seq/ListModel hybrid from a set of functions

--- src/clarity/list.clj
+++ src/clarity/list.clj
@@ -37,3 +37,61 @@
                     (reset! cache (fun new))
                     (.fireContentsChanged model model 0 (dec (count new)))))
        model)))
+
+(defprotocol ListModel
+  (append [this item])
+  (insert [this index item]))
+
+
+;;(def a (to-array [1 2 3 4]))
+;;(def s (seq a)
+;;(aset a 2 15)
+;;s
+(defn list-model
+  ([fmap] (list-model fmap 0))
+  ([fmap pointer]
+     (proxy [javax.swing.AbstractListModel
+             clarity.list.ListModel
+             clojure.lang.ISeq
+             clojure.lang.Counted
+             clojure.lang.Indexed] []
+       (getElementAt [index] ((:get fmap) index))
+       (getSize [] ((:count fmap)))
+       (append [item]
+         (do ((:add fmap) item)
+             (let [i (dec (count this))]
+               (proxy-super fireIntervalAdded this i i))))
+       (insert [index item]
+         (do ((:insert fmap) index item)
+             (proxy-super fireIntervalAdded this index index)))
+       (first [] (.getElementAt this pointer))
+       (next []
+         (if (< (inc pointer) (.getSize this))
+           (list-model fmap (inc pointer))
+           nil))
+       (more [] (let [n (next this)]
+                  (if n n
+                      clojure.lang.PersistentList/EMPTY)))
+       (cons [x] (cons x this))
+       (empty [] clojure.lang.PersistentList/EMPTY)
+       (equiv [x] false) ;;TODO
+       (seq [] this)
+       (nth
+         ([index]
+            (.getElementAt this (+ pointer index)))
+         ([index not-found]
+            (if (or (< index 0)
+                    (>= index (count this)))
+              not-found
+              (.getElementAt this (+ pointer index)))))
+       (count []
+         (- (.getSize this) pointer)))))
+
+(let [data (atom [1 2 3 4 5 6 7 8])]
+    (def m
+      (list-model
+       {:get (fn [index] (nth @data index))
+        :add (fn [item] (swap! data conj item))
+        :insert (fn [index item])
+        :count (fn [] (count @data))})))
+
